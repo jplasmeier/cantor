c259a3eaf663d6aea381b7152fe3f3be697848f7
Justin Plasmeier <jgp45@case.edu>
2017-05-12 15:38:18 -0400

    defining new models for datasets and features

--- query_api/models.py
+++ query_api/models.py
@@ -13,4 +13,20 @@ class QueryObject(models.Model):
 
     class Meta:
         ordering = ('short_description','long_description',)
+"""
+class Feature(models.Model):
+    dataset = models.ForeignKey(Dataset)
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    column_name = models.CharField(max_length=100)
+    datatype = models.CharField(max_length=40)
 
+class Dataset(models.Model):
+    short_name = models.CharField(max_length=40)
+    long_name = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=140)
+    long_description = models.TextField()
+    icon = models.CharField(max_length=40)
+    initial_feature = models.OneToOneField(Feature)
+    table_name = models.CharField(max_length=100)
+"""
